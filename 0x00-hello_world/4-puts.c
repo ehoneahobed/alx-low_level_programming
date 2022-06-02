e84b5f09adbd99a18b2e03104846a1236d2c8ad6
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-02 12:39:28 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *  main - prints the phrase "Programming is like building a multilingual puzzle
+ *  main - prints a given phrase
  *  Return: 0 if exited properly, non-zero otherwise
  */
 
