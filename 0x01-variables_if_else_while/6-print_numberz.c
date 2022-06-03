4d52c89c8ba41b708a94bd32d80da598971edf99
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-03 14:34:36 -0700

    Add solution to task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int  x;
+
+	for(x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
