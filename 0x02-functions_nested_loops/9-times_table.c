bdb81b7d799d7a9e4f34e8b7ed4f888e793291d0
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-07 13:30:27 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,8 @@ void times_table(void)
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
-
-      			if (y == 0)
+			
+			if (y == 0)
 			{
 				_putchar(z + '0');
 			}
@@ -24,8 +24,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(z + '0');
-			} 
-         		else if (z >= 10)
+			}
+			else if (z >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
