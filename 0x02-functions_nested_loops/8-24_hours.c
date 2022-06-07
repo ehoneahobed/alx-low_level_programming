780714d4350609c65cd46e77c6b1ea8d8e245082
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-07 12:10:20 -0700

    Add solution to task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 24; x++)
+	{
+		for (y = 0; y <= 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
