7f31b9bf6fc6334fb468669841899274a078a2e6
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-07 12:13:10 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 24; x++)
+	for (x = 0; x < 24; x++)
 	{
-		for (y = 0; y <= 60; y++)
+		for (y = 0; y < 60; y++)
 		{
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
