774a0bc420e0e356d8dc0b1586a6e2d3f2deae98
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-07 10:56:43 -0700

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return 0;
+		return (0);
 	}
 }
