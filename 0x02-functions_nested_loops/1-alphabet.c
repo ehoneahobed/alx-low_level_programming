1916680cedc8cc917264a5b766ba5d794190bc4f
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-07 09:55:17 -0700

    Add solution to task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
