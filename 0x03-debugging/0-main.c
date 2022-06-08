efb1fb02da15754cfbfc1c5e1bf391c8fb0691a5
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-06-08 20:37:48 +0000

    Update task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-    int i;
+        int i;
     
-    i = 0;
-    positive_or_negative(i);
-    return (0);
+        i = 0;
+        positive_or_negative(i);
+        return (0);
 }
