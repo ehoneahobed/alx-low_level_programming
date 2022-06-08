b2c2fb36b680c577a1781f65f7af5fd69d52b069
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-06-08 20:03:04 +0000

    Update 0x03-directory

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Entry point
+ * Description: tests function that prints if integer is positive or negative
+ * Return: Always  0 (success)
  */
 
 int main(void)
 {
     int i;
     
-    i = 0;
+    i = 98;
     positive_or_negative(i);
     return (0);
 }
