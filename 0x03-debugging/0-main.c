df1324e148d5bd1e6ed2be5695e8a4111dc69bfc
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-06-08 19:34:56 +0000

    Update 0x03-directory

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(void) {
-  int x = 0;
-  positive_or_negative(x);
-  return (0);
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+    int x;
+    
+    x = 0;
+    positive_or_negative(x);
+    
+    return (0);
+
 }
