1c2e74d5f74fa4c303c39ad668e8f7a35e6f174d
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-08 14:11:15 -0700

    Update task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
+	
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
 	
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
