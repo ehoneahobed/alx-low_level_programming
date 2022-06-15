d2743e517656cc9e907a7d4584f89c7b0459ac06
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-15 14:51:54 -0700

    Add solution to task 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
