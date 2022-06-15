e88db8889388d7a47f57cd39f359b553cf4f7c61
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-15 15:13:27 -0700

    Update solution to task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
