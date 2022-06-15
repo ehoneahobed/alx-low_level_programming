3a0bd7fe138200634ce61fb895b1540775a6c746
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-15 15:17:20 -0700

    Update solution to task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,60 +1,29 @@
 #include "main.h"
 
 /**
-* infinite_add - C function that adds two numbers stored
-*in strings to a buffer.
-*Assumes that strings are never empty and
-*that numbers will always be positive, or 0.
-*Assumes there are only digits stored in the number strings.
-*If result can be stored in the buffer,
-*returns a pointer to the result.
-*If result cannot be stored in the buffer, returns `0`.
-*@n1:first number to be added
-*@n2:second number to be added
-*@r: store result
-*@size_r: size of buffer
-*Return:returns pointer to result
+* print_number - prints an integer using _putchar
+* @n: integer to be printed
 */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_number(int n)
 {
-}
+	unsigned int num;
 
-/**
-* add_strings - Adds the numbers stored in two strings.
-* @n1: The string containing the first number to be added.
-* @n2: The string containing the second number to be added.
-* @r: The buffer to store the result.
-* @r_index: The current index of the buffer.
-*
-* Return: If r can store the sum - a pointer to the result.
-*         If r cannot store the sum - 0.
-*/
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-	int num, tens = 0;
-
-	for (; *n1 && *n2; n1--, n2--, r_index--)
+	/* print - infront of negative numbers */
+	if (n < 0)
 	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	for (; *n1; n1--; r_index++)
-	{
-		num = *(n1 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
+	/* print the digits infront of the current digit */
+	if (num / 10)
+		print_number(num / 10);
 
-	for (; *n2; n2--;  r_index--)
-	{
-		num = (*n2 - '0') + tens; 
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10; 
-	}
-	
+	/* print current digit */
+	_putchar((num % 10) + '0');
 }
