06209db86cc5a89eefd7922b7c7c051cbd44a46a
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-22 06:48:58 -0700

    Add solution to task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Add solutions to tasks on recurssions
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
