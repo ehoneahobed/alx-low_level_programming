43c428a374b716bc2159a79983ef1ee8924d5cb4
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-22 07:51:12 -0700

    Add solution to task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
