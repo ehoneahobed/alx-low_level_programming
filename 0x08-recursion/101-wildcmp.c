f81817f5dc3facd4dea01280e3cfe92532063270
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-22 09:19:59 -0700

    Update solution to task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,109 @@
 #include "main.h"
 
-/**
- * wildcmp - compare two strings with "wildcard expansion" capabilities
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings can be considered identical, else 0
- */
+int bandersnatch(char *s1, char *s2);
+char *move(char *s2);
 
+/**
+ * wildcmp - compares two strings recursively,
+ * checking for wildcards expansion
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if the strings can be considered identical
+ * otherwise 0
+ */
 int wildcmp(char *s1, char *s2)
 {
-	int findsrc(char *s, char c, int i, int p);
+	/**
+	 * this is going to be a sum of return values
+	 */
+	int sum = 0;
 
-	if (*s1 == '\0' && *s2 == '\0')
+	/**
+	 * if we reach the end of s1 and the char in s2 is a *
+	 * and if the next chars of s2 are *, return 1
+	 */
+	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (*s1 == '\0' && *s2 == '*')
-			return (wildcmp(s1, ++s2));
-		else if (*s1 == '*' && *s2 == '\0')
-			return (wildcmp(++s1, s2));
-		return (0);
-	}
 
+	/**
+	 * if the chars are equal in both strings,
+	 * if we reached the end of s1, return 1
+	 * else increment s1 and s2 by 1
+	 */
 	if (*s1 == *s2)
 	{
-		return (wildcmp(++s1, ++s2));
+		if (*s1 == '\0')
+			return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else if (*s1 == '*')
-	{
-		if (*(s1 + 1) == '*')
-			return (wildcmp(++s1, s2));
-		else
-		{
-			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
-		}
-	}
-	else if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, ++s2));
-		else
-		{
-			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
-		}
-	}
+	/**
+	 * if we reached the end of both strings,
+	 * return 0
+	 */
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
 
+	/**
+	 * if the char in s2 is a *
+	 * finds the address of the first char after the *
+	 * if we reached the end of s2, return 1
+	 * if the chars are equal, add the return values
+	 * of wildcmp() to sum
+	 * add the return value of bandersnatch() to sum
+	 * convert non-zero to 1, keeps 0 at 0, return
+	 */
+	if (*s2 == '*')
+	{
+		s2 = move(s2);
+		if (*s2 == '\0')
+			return (1);
+		if (*s1 == *s2)
+			sum += wildcmp(s1 + 1, s2 + 1);
+		sum += bandersnatch(s1 + 1, s2);
+		return (!!sum);
+	}
 	return (0);
-
 }
 
 /**
- * findsrc - helper function
- * @s: - string to iterate
- * @c: - character to
- * @i: - integer to
- * @p: - integer to
- * Return: - returns ...
+ * bandersnatch - checks recursively for all the paths when the
+ * characters are equal
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return value of wildcmp() or of itself
  */
-
-int findsrc(char *s, char c, int i, int p)
+int bandersnatch(char *s1, char *s2)
 {
-	if (*(s + i) == '\0')
-		return (p + 1);
-	else if (*(s + i) == c || *(s + i) == '*')
-		p = i;
+	/**
+	 * if we reached the end of s1, return 0
+	 * if chars are equal, return the return value of wildcmp()
+	 * increment s1 by 1, not s2
+	 */
+	if (*s1 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1, s2));
+	return (bandersnatch(s1 + 1, s2));
+}
 
-	return (findsrc(s, c, i + 1, p));
+/**
+ * *move - moves the current char past the *
+ * @s2: string to iterate over
+ *
+ * Return: the address of the character after the *
+ */
+char *move(char *s2)
+{
+	/**
+	 * if the current char is a *
+	 * increment s2 by 1
+	 * else return the address of
+	 * the first char past all *
+	 */
+	if (*s2 == '*')
+		return (move(s2 + 1));
+	else
+		return (s2);
 }
