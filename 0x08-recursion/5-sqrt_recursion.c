c067a2f46b516301f895f199e61aecb29179b67d
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-22 08:35:10 -0700

    Add solution to task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int sqrt_fun(int n,int sq);
+
+	int sq = 1;
+
+	return sqrt_fun(n, sq);
+}
+
+/**
+ * halp - helper function to solve _sqrt_recursion
+ * @sq: number to determine if square root
+ * @n:  int number
+ * Return: square root if natural square root, or -1 if none found
+ */
+
+int sqrt_fun(int n,int sq)
+{
+	if (n == sq * sq)
+	{
+		return (sq);
+	}
+	else if (sq < n)
+	{
+		return sqrt_fun(n, ++sq);
+	}
+	else
+	{
+		return (-1);
+	}
+}
