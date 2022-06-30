64af160d005212b083bf318da5f7a130e3946da5
ehoneahobed <ehoneahobed@hotmail.com>
2022-06-30 08:34:54 -0700

    Add solution to task zero

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
