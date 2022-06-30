17de785dceb9a504c4022765b0bd488658838904
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-06-30 22:52:55 +0000

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
+
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 int *array_range(int min, int max);
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int _putchar(char c);
+
+int if_number(char *s);
+
+unsigned int string_length(char *s);
+
+void print_string(char *s);
 
 #endif
