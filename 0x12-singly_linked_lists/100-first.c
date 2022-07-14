b35deb711c30516f044f8c0d150670f5b98c3b4a
ehoneahobed <ehoneahobed@hotmail.com>
2022-07-14 15:02:29 -0700

    Add solution to task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
