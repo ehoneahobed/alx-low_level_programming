9c8ef07caabcd1f4d38f4ed41ac0af8eb75e25ed
ehoneahobed <ehoneahobed@hotmail.com>
2022-07-19 09:08:01 -0700

    Update solution to task 0 for betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-    int count = 0;
+	int count = 0;
 
-    for (; h != NULL; h = h->next)
-    {
-        printf("%d\n", h->n);
-        count++;
-    }
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
