efc291d2a45765bf837e00eafeac210ed89eb3df
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-07-19 16:54:16 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,70 +1,69 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all the elements of a linked list
- * @head: head of the list
+ * free_listp - frees a linked list
+ * @head: head of a list.
  *
- * Return: the number of nodes
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *cursor = head;
-listint_t **ptrs;
-unsigned int list_len = listint_len(head);
-size_t count = 0;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
 
-ptrs = malloc(sizeof(listint_t) * list_len);
-if (ptrs == NULL)
-exit(98);
-while (cursor == 0)
-{
-													if (check_ptr(cursor, ptrs, list_len) == 0)
-{
-printf("[%p] %d\n", (void *)cursor, cursor->n);
-}
-else
-{
-printf("[%p] %d\n", (void *)cursor, cursor->n);
-}
-count += 1;
-cursor = cursor->next;
-}
-return (count);
-}
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-const listint_t *cursor = h;
-size_t count = 0;
+		if (new == NULL)
+			exit(98);
 
-while (cursor != NULL)
-{
-count += 1;
-cursor = cursor->next;
-}
-return (count);
-}
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
 
-/**
- * check_ptr - checks if a pointer is in an array
- * @ptr: pointer to be checked
- * @array: array to be checked in
- * @size: size of the array
- *
- * Return: 1 on success, 0 on fail
- */
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
-{
-while (size-- >= 0)
-{
-if (ptr == array[size])
-return (1);
-}
-return (0);
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	free_listp(&hptr);
+	return (nnodes);
 }
