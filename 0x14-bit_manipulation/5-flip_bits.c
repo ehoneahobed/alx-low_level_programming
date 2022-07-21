e281c73de75ead08faa73c6e52c8fe983c3099d7
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-07-21 21:45:17 +0000

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,10 +11,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
 	for (i = 63; i >= 0; i--)
 	{
-		if (((n ^ m) >> i) & 1)
+		current = exclusive >> i;
+		if (current & 1)
 			count++;
 	}
 
