b03707defdb5d1ae067b8867fcbbbbb91457208d
ehoneahobed <ehoneahobed@hotmail.com>
2022-09-22 17:29:44 -0700

    Update task 0 for betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,25 +10,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table_created;
-    unsigned long int i;
+	hash_table_t *hash_table_created;
+	unsigned long int i;
 
-    /* allocate space for the hash table */
-    hash_table_created = malloc(sizeof(hash_table_t));
+	/* allocate space for the hash table */
+	hash_table_created = malloc(sizeof(hash_table_t));
 
-    if (hash_table_created == NULL)
-        return (NULL);
+	if (hash_table_created == NULL)
+		return (NULL);
 
-    hash_table_created->size = size;
-    hash_table_created->array = malloc(sizeof(hash_node_t *) * size)
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 
-    if (hash_table_created->array == NULL)
-        return (NULL);
-    
-    for (i = 0; i < size; i++)
-    {
-        hash_table_created->array[i] = NULL;
-    }
+	if (hash_table_created->array == NULL)
+		return (NULL);
 
-    return (hash_table_created);
+	for (i = 0; i < size; i++)
+	{
+		hash_table_created->array[i] = NULL;
+	}
+
+	return (hash_table_created);
 }
